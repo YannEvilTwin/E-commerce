261b98f012802c8ea1dd8fa83ff7c880f11c807e
Yanina Andregnette <62681163+YannEvilTwin@users.noreply.github.com>
2021-07-24 00:40:57 -0300

    Actualizacion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ console.log(props);
         <nav>
             <h1>{props.nombreDeLaTienda}</h1>   
             <ul>
-                <li><Link>Inicio</Link></li>
+                <li><Link to="">Inicio</Link></li>
                 <li><Link to="/category/1">Combos</Link></li>
                 <li><Link to="/category/2">Perifericos</Link></li>
                 <li><Link>Contacto</Link></li>
