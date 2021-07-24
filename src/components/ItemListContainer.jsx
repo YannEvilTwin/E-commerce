549a3057b3ccc364c1f6abaecb1d1f0646912640
Yanina Andregnette <62681163+YannEvilTwin@users.noreply.github.com>
2021-07-24 00:10:12 -0300

    Actualizacion filtrado de productos por categoria

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -9,7 +9,7 @@ const itemsArray = [
     {
         id: 1,
         nombre: "Gamer Kit Redragon",
-        imagen: "./images/1.png",
+        imagen: "../images/1.png",
         precio: 8699,
         category: 1,
         stock: 10,
@@ -17,7 +17,7 @@ const itemsArray = [
     {
         id: 2,
         nombre: "Gamer Bluecase BG-009 ATX",
-        imagen: "./images/2.png",
+        imagen: "../images/2.png",
         precio: 12237,
         category: 2,
         stock: 10,
@@ -25,7 +25,7 @@ const itemsArray = [
     {
         id: 3,
         nombre: "Gamer Headset Redragon",
-        imagen: "./images/3.png",
+        imagen: "../images/3.png",
         precio: 6500,
         category: 2,
         stock: 10,
@@ -33,7 +33,7 @@ const itemsArray = [
     {
         id: 4,
         nombre: "Gamer Mouse 3200dpi",
-        imagen: "./images/4.png",
+        imagen: "../images/4.png",
         precio: 1850,
         category: 2,
         stock: 10,
@@ -67,7 +67,7 @@ const ItemListContainer = (props) => {
            
     useEffect (() => {
         obtenerItems() .then (respuesta => setProductos (respuesta))
-    },[])
+    },[id])
 
 
     return (
