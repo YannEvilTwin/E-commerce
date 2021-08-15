1dce701220eede30009fc0290fb771466d25fe18
Yanina Andregnette <62681163+YannEvilTwin@users.noreply.github.com>
2021-08-15 01:52:11 -0300

    Actualizacion Form

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, getFirestore } from 'react';
 
 //se crea el Contexto para ser usado en cualquier componente
 //se genera un export const porque lo tengo que importar cada vez que lo quiera usar
@@ -6,17 +6,13 @@ export const CartContext = createContext();
 
 //creo mi componente que va a contener mi estado global, el cual sera utilizado en App.js
 export const CartProvider = ({ children }) => {
-
-  
     const usuario = useState(null); 
-    
     const [cart, setCart] = useState ([]); //este useState se va a transformar en mi estado global
-
     const [cartLength, setCartLength] = useState(0);
+    const [total, setTotal] = useState(0);
 
     //funciones que modificaran el cart
-  
-    //Agrega al carrito
+   //Agrega al carrito
     const addItem = (item) => {
         setCart([...cart, item])
     };
@@ -44,7 +40,7 @@ export const CartProvider = ({ children }) => {
         setCart([])
     };
 
-     //funcion para remover un Item del carrito
+    //funcion para remover un Item del carrito
     const deleteItem = (id) => {
     const newCart = cart.filter((item) => item.id !== id);
     setCart(newCart);
@@ -59,6 +55,12 @@ export const CartProvider = ({ children }) => {
         return setCartLength(cartAux)
     } , [cart]);
 
+    const createOrder = (email, phone, name) => {
+     const order = {buyer: { email, phone, name }, item: cart, total: total}  
+     const db = getFirestore();
+     db.collection('orders').add(order);
+    }
+
     //creo un objeto para ir agregando todo lo que quiera tener en mi estado global
    //puedo pasar array, objetos o funciones
     const values = {
@@ -70,7 +72,9 @@ export const CartProvider = ({ children }) => {
     addItem,
     deleteItem,
     agregarAlCarrito,
-    cartLength
+    cartLength,
+    createOrder,
+    total,
     };
 
     //retorno el context provider y en value le asigno los valores de mi estado global
--- src/components/Navbar/Cart.jsx
+++ src/components/Navbar/Cart.jsx
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../CartContext/CartContext";
 import Item from "../Item/Item";
 import ItemCart from "../ItemCart/ItemCart";
 
 const Cart = () => {
-    
+    const [name, setName] =useState(''); 
+    const [email, setEmail] =useState(''); 
+    const [phone, setPhone] =useState(''); 
     
     const { cart } = useContext(CartContext);
-    const { vaciarCarrito, cartLength } = useContext (CartContext);
+    const { vaciarCarrito, cartLength, createOrder} = useContext (CartContext);
     const handleVaciarCarrito = () => {
         vaciarCarrito(
 
@@ -17,6 +19,7 @@ const Cart = () => {
     const total = cart.reduce((acc, {cantidad, precio}) => {
         return acc = acc + (precio * cantidad)
     }, 0);
+
 
     return (
         cartLength > 0 ? 
@@ -28,8 +31,16 @@ const Cart = () => {
             </div>
         </div>
         <h3>Total: {total}</h3>
+        <div>
+            <input type="email" onImput={(e) => {setEmail(e.target.value)}} placeholder="email"/>
+            <input type="tel" onImput={(e) => {setPhone(e.target.value)}} placeholder="phone"/>
+            <input type="text" onImput={(e) => {setName(e.target.value)}} placeholder="name"/>
+
+        </div>
         <div> 
-            <button onClick={vaciarCarrito} className="">Vaciar Carrito</button>
+            <button onClick={() => {vaciarCarrito()}}>Vaciar Carrito</button>
+            <button onClick={() => {createOrder(name, email, phone)}}>Comprar</button>
+            
         </div>     
         </>
         : <h3 className="textoCentrado">No hay productos en el carrito</h3>
